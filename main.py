81442687c6c5bf6de3a3ff080be727a131c0044a
IvanDS <ivan15_m_a@hotmail.com>
2020-06-07 14:05:06 +0200

    update gitignore and main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-for x in xrange(1, 200):
+for x in range(1, 200):
  
     fizz = not x % 3
     buzz = not x % 5
  
     if fizz and buzz :
-        print "FizzBuzz"
+        print("FizzBuzz")
     elif fizz:
-        print "Fizz"
+        print("Fizz")
     elif buzz:
-        print "Buzz"
+        print("Buzz")
     else:
-        print x
+        print(x)
